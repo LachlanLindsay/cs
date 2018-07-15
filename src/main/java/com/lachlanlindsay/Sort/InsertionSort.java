18dd6651dd5412891da0033ffa42e37f49f39363
Lachlan Lindsay <lindsaylachlan@gmail.com>
2018-07-15 15:55:21 +1000

    changes insertion sort to use recursion

--- src/main/java/com/lachlanlindsay/Sort/InsertionSort.java
+++ src/main/java/com/lachlanlindsay/Sort/InsertionSort.java
@@ -3,31 +3,33 @@ package com.lachlanlindsay.Sort;
 /**
  * quadratic sorting algorithm
  * More efficient in practice than most other simple quadratic (i.e., O(n2)) algorithms such as selection sort or bubble sort
- * Adaptive, i.e. efficient for data sets that are already substantially sorted: the time complexity is O(nk) when each element in the input is no more than k places away from its sorted position
+ * Adaptive, i.e. efficient for data sets that are already substantially sorted: the time complexity is O(nk) when each
+ * element in the input is no more than k places away from its sorted position
  * Stable; i.e. does not change the relative order of elements with equal keys
- * In-place; i.e. only requires a constant amount O(1) of additional memory spac
+ * In-place; i.e. only requires a constant amount O(1) of additional memory space
  * <p>
  * https://en.wikipedia.org/wiki/Insertion_sort
  */
 public class InsertionSort {
-
-    /*
-     * outer loop is growing partition by one.
-     * inner loop is looking for correct place to insert value and moves the array by one.
-     */
     public static void sort(int[] array) {
+        insertionSort(array, array.length);
+    }
 
-        //index zero is already considered sorted
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex < array.length; firstUnsortedIndex++) {
-            int temp = array[firstUnsortedIndex];
-
-            int i;
-            //if not at beginning of array && the previous element contains a larger value
-            for (i = firstUnsortedIndex; i > 0 && array[i - 1] > temp; i--) {
-                //working within the sorted partition from right to left
-                array[i] = array[i - 1];
-            }
-            array[i] = temp;
+    private static void insertionSort(int[] input, int numItems) {
+        if (numItems < 2) {
+            return;
         }
+
+        insertionSort(input, numItems - 1);
+
+        int newElement = input[numItems - 1];
+
+        int i;
+        //if not at beginning of array && the previous element contains a larger value
+        for (i = numItems - 1; i > 0 && input[i - 1] > newElement; i--) {
+            //working within the sorted partition from right to left
+            input[i] = input[i - 1];
+        }
+        input[i] = newElement;
     }
 }
--- src/test/java/com/lachlanlindsay/sort/InsertionSortTest.java
+++ src/test/java/com/lachlanlindsay/sort/InsertionSortTest.java
@@ -10,6 +10,7 @@ public class InsertionSortTest {
     @Test
     public void testSort() {
         int[] unsortedArray = {4, -5, 22, 55, 33, 44, 5};
+
         final int[] sortedArray = {-5, 4, 5, 22, 33, 44, 55};
         sort(unsortedArray);
         assertArrayEquals(sortedArray, unsortedArray);
