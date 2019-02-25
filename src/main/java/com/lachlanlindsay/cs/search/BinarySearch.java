1f6e14793a3a6866e88dafc6e2d450fe2d8997ae
Lachlan Lindsay <lindsaylachlan@gmail.com>
2019-02-25 23:16:08 +1000

    adds binary search

---
+++ src/main/java/com/lachlanlindsay/cs/search/BinarySearch.java
@@ -0,0 +1,33 @@
+package com.lachlanlindsay.cs.search;
+
+public class BinarySearch {
+
+    // 1, 2, 3, 4, 5, 6, 7, 8
+
+    /**
+     * returns the index that the target element can be found at. If it does not exist return -1
+     */
+    public static int iterativeBinarySearch(int[] input, int target) {
+        int start = 0;
+        int end = input.length;
+        int midpoint;
+
+
+        while (start < end) {
+            int midPoint = (start + end) / 2;
+
+            if (input[midPoint] == target) {
+                return midPoint;
+            }
+
+            if (input[midPoint] > target) {
+                end = midPoint;
+            } else if (input[midPoint] < target) {
+                start = midPoint;
+            }
+        }
+
+        return -1;
+
+    }
+}
---
+++ src/test/java/com/lachlanlindsay/cs/search/BinarySearchTest.java
@@ -0,0 +1,19 @@
+package com.lachlanlindsay.cs.search;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BinarySearchTest {
+
+    @Test
+    public void test() {
+        int[] indexes = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] complex = {14, 49, 548, 892034};
+
+        int i = BinarySearch.iterativeBinarySearch(indexes, 49);
+        assertEquals(0, BinarySearch.iterativeBinarySearch(indexes, 0));
+        assertEquals(1, BinarySearch.iterativeBinarySearch(complex, 49));
+    }
+
+}
